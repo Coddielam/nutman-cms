1b341d43581f1de3e33ffdd6342c061f427a5e9e
Eddie Lam <eddie.cm.lam@gtomato.com>
2023-01-11 14:45:55 +0800

    update config for railway

--- config/database.js
+++ config/database.js
@@ -1,25 +1,39 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "localhost"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", ""),
+//       user: env("DATABASE_USERNAME", ""),
+//       password: env("DATABASE_PASSWORD", ""),
+//     },
+//     acquireConnectionTimeout: 600000,
+//     pool: {
+//       min: 0,
+//       max: 100,
+//       acquireTimeoutMillis: 300000,
+//       createTimeoutMillis: 300000,
+//       destroyTimeoutMillis: 50000,
+//       idleTimeoutMillis: 300000,
+//       reapIntervalMillis: 10000,
+//       createRetryIntervalMillis: 2000,
+//       propagateCreateError: false,
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", ""),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5432),
+      database: env("PGDATABASE", "strapi"),
+      user: env("PGUSER", "strapi"),
+      password: env("PGPASSWORD", "password"),
+      ssl: env.bool(true),
     },
-    acquireConnectionTimeout: 600000,
-    pool: {
-      min: 0,
-      max: 100,
-      acquireTimeoutMillis: 300000,
-      createTimeoutMillis: 300000,
-      destroyTimeoutMillis: 50000,
-      idleTimeoutMillis: 300000,
-      reapIntervalMillis: 10000,
-      createRetryIntervalMillis: 2000,
-      propagateCreateError: false,
-    },
-    useNullAsDefault: true,
   },
 });
--- config/plugins.js
+++ config/plugins.js
@@ -1,23 +1,45 @@
+// module.exports = ({ env }) => ({
+//   upload: {
+//     config: {
+//       provider: "aws-s3",
+//       providerOptions: {
+//         accessKeyId: env("AWS_ACCESS_KEY_ID"),
+//         secretAccessKey: env("AWS_ACCESS_SECRET"),
+//         region: env("AWS_REGION"),
+//         params: {
+//           Bucket: env("AWS_BUCKET_NAME"),
+//         },
+//       },
+//       // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
+//       actionOptions: {
+//         upload: {
+//           ACL: null,
+//         },
+//         uploadStream: {
+//           ACL: null,
+//         },
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
   upload: {
     config: {
-      provider: "aws-s3",
+      provider: "cloudinary",
       providerOptions: {
-        accessKeyId: env("AWS_ACCESS_KEY_ID"),
-        secretAccessKey: env("AWS_ACCESS_SECRET"),
-        region: env("AWS_REGION"),
-        params: {
-          Bucket: env("AWS_BUCKET_NAME"),
-        },
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
-      // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
       actionOptions: {
-        upload: {
-          ACL: null,
-        },
-        uploadStream: {
-          ACL: null,
-        },
+        upload: {},
+        delete: {},
       },
     },
   },
